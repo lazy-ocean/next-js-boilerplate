bf789b9470256952e32954ed66b99d91abe7ccc7
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-06-07 17:55:30 +0200

    build: add styled-components ssr support + theme to start from

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import "../../styles/globals.css";
 import Main from "../components/Main/Main";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "../theme";
 
 const MyApp = () => {
   /* let testLintIsWorking = ""; */
-  return <Main />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Main />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
