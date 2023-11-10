bb0547d423d45923b8643d4e0605a08d6fea13b5
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2023-11-10 18:37:22 +0100

    build: update next.js, move to app directory, add fancy api for RSC showcase

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.metmuseum.org",
+      },
+    ],
   },
 };
 
